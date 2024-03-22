4ca9d98a8df5b73b7ee41a9c5649916c7ab07581
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-22 15:05:39 +0300

    complete challange1

---
+++ challange1.js
@@ -0,0 +1,18 @@
+function studentGradeCalc() {
+    let studentGrade = ''  // declares studentGrade in a manner that allows it to change
+    if (studentMark > 79 && studentMark <= 100) {
+        studentGrade == 'A'
+    }
+    else if (studentMark >= 60 && studentMark <= 79) {
+        studentGrade == 'B'
+    }
+    else if (studentMark >= 50 && studentMark <= 59) {
+        studentGrade == 'C'
+    }
+    else if (studentMark >= 40 && studentMark <= 49) {
+        studentGrade == 'B'
+    }
+    else {
+        studentGrade == 'E'
+    }
+} // if..else if...else used to return the correct grade
