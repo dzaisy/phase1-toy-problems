d38b8c18ae1d2825234de6f0d4cdf466b9d97e56
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-24 14:58:14 +0300

    final challange3

--- challange3.js
+++ challange3.js
@@ -1,2 +1,16 @@
 // challange 3 - net salary calc
-function calcNetSal() {}
+function netSal() {
+    let basicSal = parseFloat(document.getElementById('basicSal').value);
+    let benefits = parseFloat(document.getElementById('banefits').value);
+    // allows user to input both basic salary and benefits
+    //  parseFloat converts a string to a floating-point number
+
+    let tax = Math.round((basicSal + benefits) * .15);
+    let NHIFDeductions = Math.round((basicSal + benefits) * .0275);
+    let NSSFDeductions = Math.round((basicSal + benefits) * .06);
+    let grossSal = Math.round((basicSal + benefits));
+    let netSal = grossSal - tax - NHIFDeductions -  NSSFDeductions;
+// tax, nhif, nssf, grosssal, netsal calculated
+// Math.round used to round numbers to whole numbers
+    document.getElementById('netSal').textContent = `net salary is: ${netSal}`; // .textContent returns the netsal
+}
