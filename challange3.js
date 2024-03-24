000bfb4b54e05af11942b4ee537ddb6b62c3eb79
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-24 23:11:42 +0300

    completed challange 3

--- challange3.js
+++ challange3.js
@@ -5,12 +5,11 @@ function netSal() {
     // allows user to input both basic salary and benefits
     //  parseFloat converts a string to a floating-point number
 
-    let tax = Math.round((basicSal + benefits) * .15);
-    let NHIFDeductions = Math.round((basicSal + benefits) * .0275);
-    let NSSFDeductions = Math.round((basicSal + benefits) * .06);
-    let grossSal = Math.round((basicSal + benefits));
-    let netSal = grossSal - tax - NHIFDeductions -  NSSFDeductions;
+    let tax = (basicSal + benefits) * .15; // tax rate is 15%
+    let NHIFDeductions = (basicSal + benefits) * .0275; // nhif rate is 2.75%
+    let NSSFDeductions = (basicSal + benefits) * .06; // nssf rate is 6%
+    let grossSal = basicSal + benefits;
+    let netSal = grossSal - (tax + NHIFDeductions +  NSSFDeductions);
 // tax, nhif, nssf, grosssal, netsal calculated
-// Math.round used to round numbers to whole numbers
-    document.getElementById('netSal').textContent = `net salary is: ${netSal}`; // .textContent returns the netsal
+    console.log(`net salary: ${netSal}`);
 }
